c407db110d9323733f7547c3daf8fcf71e525d24
Edson Candido <edson.cursoprogramacao@gmail.com>
2020-10-12 22:47:24 -0300

    Inicio do projeto Happy, criação da Landing Page

---
+++ src/pages/Landing.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { FiArrowRight } from 'react-icons/fi';
+
+import '../styles/pages/landing.css';
+
+import logoImg from '../images/logo.svg';
+
+function Landing() {
+    return(
+        <div id="page-landing">
+        <div className="content-wrapper">
+          <img src={logoImg} alt="Happy"/>
+  
+          <main>
+            <h1>Leve felicidade para o mundo</h1>
+            <p>Visite orfanatos e mude o dia de muitas crianças.</p>
+          </main>
+  
+          <div className="location">
+            <strong>Camboriú</strong>
+            <span>Santa Catarina</span>
+          </div>
+  
+          <a href="" className="enter-app">
+            <FiArrowRight size={26} color="rgba(0, 0, 0, 0.6)" />
+          </a>
+        </div>
+      </div>
+  
+    );
+}
+
+export default Landing;
